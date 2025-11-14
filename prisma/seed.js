acbe5e803e0f781bdc01d1b328e703059f6fa5e8
vikat.gen <vikat.gen@gmail.com>
2025-11-14 14:58:07 +0200

    feature: adds authors as fake data and connects with book resource.

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,13 +2,34 @@ import prisma from "../src/config/prisma.js";
 import { faker } from "@faker-js/faker/locale/en";
 
 const databaseSeeder = async () => {
+    const authors = [];
+
+    for (let i = 0; i < faker.number.int({ min: 30, max: 50}); i++) {
+        const author = await prisma.author.create({
+            data: {
+                name: faker.person.fullName()
+            }
+        });
+
+        authors.push(author);
+    }
+
     for(let i = 0; i < 50; i++) {
+        const randomAuthor = authors[faker.number.int({ min: 0, max: authors.length - 1})];
+
         const book = await prisma.book.create({
             data: {
                 title: faker.book.title(),
                 description: faker.lorem.sentence(),
                 thumbnail_url: faker.image.url(),
                 release_year: new Date(faker.date.anytime()).getFullYear(),
+                authors: {
+                    create: [
+                        {
+                            author: { connect: { id: randomAuthor.id } }
+                        }
+                    ]
+                }
             }
         })
     }
