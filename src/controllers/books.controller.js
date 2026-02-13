1bba0952c3c7f539d2561723b410e642bae0b427
vikatgen <vikat.gen@gmail.com>
2026-02-13 14:54:23 +0200

    refactor: changed book module functional to OOP principles for improving testing

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -1,125 +1,53 @@
-import prisma from '../config/prisma.js';
-import { QueryBuilder } from "../utils/QueryBuilder.js";
-import NotFoundError from "../utils/NotFoundError.js";
+class BookController {
 
-export const getAllBooks = async (request, response, next) => {
-    try {
-        const Builder = new QueryBuilder(request.query, {
-            defaultSort: 'created_at',
-            defaultTake: 20,
-            allowedSorts: ['id', 'title', 'description', 'created_at', 'updated_at'],
-            allowedSearchFields: ['title', 'description'],
-            allowedIncludes: {
-                'authors': { include: { author: true }}
-            }
-        });
-
-        const prismaQuery = Builder.buildPrismaQuery();
-
-        const [books, count] = await Promise.all([
-            prisma.book.findMany(prismaQuery),
-            prisma.book.count({ where: prismaQuery.where })
-        ]);
-
-        const meta = Builder.getPaginationMeta(count);
-
-        response.status(200).json({
-            message: 'All books',
-            data: books,
-            meta,
-        });
-    } catch (exception) {
-        next(exception);
+    constructor(bookService) {
+        this.bookService = bookService;
     }
-};
 
-export const getBookById = async (request, response, next) => {
-    try {
-        const idFromURL = request.params?.id;
-
-        const book = await prisma.book.findUnique({
-            where: {
-                id: Number(idFromURL)
-            }
-        });
-
-        if (!book) throw new NotFoundError(`Book with id ${idFromURL} not found`);
-
-        response.status(200).json({
-            message: 'Successfully Found Book',
-            data: book
-        })
-    } catch (exception) {
-        next(exception);
-    }
-};
-
-export const createBook = async (request, response, next) => {
-    try {
-        const { title, description, thumbnail_url, release_year } = request.body;
-
-       await prisma.book.create({
-            data: {
-                title,
-                description,
-                thumbnail_url,
-                release_year: Number(release_year),
-            }
-        });
-
-        response.sendStatus(201);
-    } catch (exception) {
-        next(exception);
-    }
-};
-
-export const updateBook = async (request, response, next) => {
-    try {
-        const { id } = request.params;
-        const { title, description, thumbnail_url, release_year } = request.body;
-
-        const updatedBook = await prisma.book.update({
-            where: {
-                id: Number(id),
-            },
-            data: {
-                title,
-                description,
-                thumbnail_url,
-                release_year: Number(release_year),
-            }
-        });
-
-        if (!updatedBook) {
-            response.status(404).json({
-                message: 'Not Found'
-            })
+    async index(request, response, next) {
+        try {
+            const books = await this.bookService.getBooks(request.query);
+            response.status(200).json(books);
+        } catch (exception) {
+            next(exception);
         }
-
-        response.status(200).json({
-            message: 'Successfully Updated Book',
-            data: updatedBook
-        })
-
-    } catch (exception) {
-        next(exception);
     }
-};
 
-export const deleteBook = async (request, response, next) => {
-    try {
-        const bookId = request.params?.id;
-
-        await prisma.book.delete({
-            where: {
-                id: Number(bookId)
-            }
-        })
-
-        response.status(200).json({
-            message: 'Successfully Deleted',
-        })
-    } catch (exception) {
-        next(exception);
+    async edit(request, response, next) {
+        try {
+            const book = await this.bookService.getBookById(request.params.id);
+            response.status(200).json(book);
+        } catch (exception) {
+            next(exception);
+        }
     }
-};
+
+    async create(request, response, next) {
+        try {
+            await this.bookService.createBook(request.body);
+            response.sendStatus(201);
+        } catch (exception) {
+            next(exception);
+        }
+    }
+
+    async update(request, response, next) {
+        try {
+            await this.bookService.updateBook(request.params.id, request.body);
+            response.sendStatus(200);
+        } catch (exception) {
+            next(exception);
+        }
+    }
+
+    async delete(request, response, next) {
+        try {
+            await this.bookService.deleteBook(request.params.id);
+            response.sendStatus(204);
+        } catch (exception) {
+            next(exception);
+        }
+    }
+}
+
+export default BookController;
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,16 +1,24 @@
 import { Router } from 'express';
 import { validateBody, validateQuery } from "../middlewares/validator.middleware.js";
 import { bookSchema } from "../validations/book.validation.js";
-import * as BookController from "../controllers/books.controller.js";
 import { bookQuerySchema } from "../validations/bookQuery.validation.js";
-import {authenticateToken} from "../middlewares/auth.middleware.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
+import prisma from "../config/prisma.js";
+
+import BookRepository from "../repository/bookRepository.js";
+import BookService from "../service/bookService.js";
+import BooksController from "../controllers/books.controller.js";
+
+const repository = new BookRepository(prisma);
+const service = new BookService(repository);
+const controller = new BooksController(service);
 
 const router = Router();
 
-router.get('/books', validateQuery(bookQuerySchema) ,BookController.getAllBooks);
-router.get('/books/:id', BookController.getBookById);
-router.post('/books', authenticateToken, validateBody(bookSchema), BookController.createBook);
-router.put('/books/:id', authenticateToken, validateBody(bookSchema), BookController.updateBook);
-router.delete('/books/:id', authenticateToken, BookController.deleteBook);
+router.get('/books', validateQuery(bookQuerySchema) ,controller.index.bind(controller));
+router.get('/books/:id', controller.edit.bind(controller));
+router.post('/books', authenticateToken, validateBody(bookSchema), controller.create.bind(controller));
+router.put('/books/:id', authenticateToken, validateBody(bookSchema), controller.update.bind(controller));
+router.delete('/books/:id', authenticateToken, controller.delete.bind(controller));
 
 export default router;
