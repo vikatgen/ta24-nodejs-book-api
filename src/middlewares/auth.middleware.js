c994b71820cc0c4549a7520805ddbb15ca5f4636
vikatgen <vikat.gen@gmail.com>
2026-02-14 17:52:52 +0200

    fix: check for null for user-exists findUnique database call

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,6 +10,7 @@ export const authenticateToken = async (request, response, next) => {
 
             const payload = jwt.verify(token, process.env.JWT_SECRET);
             const user = await prisma.user.findUnique({ where: { id: payload.id } });
+            if (!user) throw new AuthenticationError("Invalid token");
 
             request.user = {
                 id: user.id,
@@ -21,46 +22,3 @@ export const authenticateToken = async (request, response, next) => {
         next(exception);
     }
 };
-
-/*
-export const authenticateToken = async (request, response, next) => {
-    try {
-        const authHeader = request.headers.authorization;
-
-        // Ootame vormingut: "Bearer <token>"
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return response.status(401).json({
-                message: 'Unauthorized'
-            });
-        }
-
-        const token = authHeader.slice(7); // eemaldab "Bearer "
-
-        let payLoad;
-        try {
-            payLoad = jwt.verify(token, process.env.JWT_SECRET);
-        } catch (err) {
-            // Vale / aegunud token → 401, mitte 500
-            return response.status(401).json({
-                message: 'Invalid token'
-            });
-        }
-
-        const user = await prisma.user.findUnique({ where: { id: payLoad.id } });
-
-        if (!user) {
-            return response.status(401).json({
-                message: 'Unauthorized'
-            });
-        }
-
-        request.user = {
-            id: user.id,
-            email: user.email
-        };
-
-        next();
-    } catch (exception) {
-        next(exception);
-    }
-};*/
